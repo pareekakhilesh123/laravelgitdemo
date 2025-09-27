73222f6e8becd09dfac854d1796c9d5f60a8d7cb
pareekakhilesh123 <pareekakhilesh5@gmail.com>
2025-09-27 09:52:57 +0530

    project setup

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+
+    <div class="div">
+ <x-dropdown-link href="{{ route('profile.show') }}">
+                                {{ __('Profile') }}
+                            </x-dropdown-link>
+
+    <h2>Jai Shree Ram</h2>
+    
+    </div>
